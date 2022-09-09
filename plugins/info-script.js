5bc0f790219075f62d2bbf3d11dd54f128f3c50f
XOrcistGans <112866322+XOrcistGans@users.noreply.github.com>
2022-09-09 08:52:34 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -11,14 +11,14 @@ https://github.com/ImYanXiao/Elaina-MultiDevice
 Pengen yang udah di recode oleh owner?
 Script bot ini dijual bila kalian mau bisa chat owner
 
-wa.me/6282127487538`
+wa.me/6283149798467`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/Kingdomidn_",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    title: 'XOrcistGans-DEV-V1 MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
