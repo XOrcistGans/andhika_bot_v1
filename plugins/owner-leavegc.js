eb85d8b52e788676cdc25c0a31b5e2d8884ad7af
XOrcistGans <112866322+XOrcistGans@users.noreply.github.com>
2022-09-09 09:31:05 +0700

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args, command }) => {
 	let group = m.chat
-        await m.reply('Sayonara , , ! (≧ω≦)ゞ', m.chat) 
+        await m.reply('Sayonara Selamat Tinggal Beban, Semoga Tenang Di Alam Sana , , ! (≧ω≦)ゞ', m.chat) 
         await  conn.groupLeave(group)
         }
 handler.help = ['leavegc', 'out']
