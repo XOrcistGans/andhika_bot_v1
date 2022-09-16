8b5c89dc0f3b512ce44ae011bd7ea42262adb9e3
XOrcistGans <112866322+XOrcistGans@users.noreply.github.com>
2022-09-16 21:16:40 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*https://youtube.com/channel/UCWUQQiIXZlni9OF-kXtbNlA*
 
 Pengen mentahan nya ?
 Nih ada santai bro....
-https://github.com/ImYanXiao/Elaina-MultiDevice
+Mau sc jangan nyolong bro gw cape, chat gw wa.me/+6283149798467
 
 Pengen yang udah di recode oleh owner?
 Script bot ini dijual bila kalian mau bisa chat owner
